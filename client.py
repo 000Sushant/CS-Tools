5754562f7db66ae943eca3e175c4f65e3443cad6
Sushant Kumar <60563287+000Sushant@users.noreply.github.com>
2023-01-10 12:25:06 +0530

    Delete client.py

--- client.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-sock_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #STREAM is for tcp and DGRAM is for udp
-sock_.connect((socket.gethostname(),4444))
-msg = sock_.recv(1024) #1024 bytes
-print(msg.decode("ascii"))
